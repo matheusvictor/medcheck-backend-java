99c194ec25645299697c1fbcee51b7680d60bd74
matheus victor <matheusvictor.salles@gmail.com>
2025-01-30 18:06:17 -0300

    refactor: database scripts

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,2 +1,11 @@
 INSERT INTO users (name, email, password)
-VALUES ('John Doe', 'john.doe@example.com', 'senha123');
+VALUES ('John Doe', 'john.doe@example.com', 'password123');
+
+INSERT INTO users (name, email, password)
+VALUES ('Mary Doe', 'mary.doe@example.com', 'password123');
+
+INSERT INTO doctors (user_id, crm, specialty)
+VALUES (1, '123', 'example');
+
+INSERT INTO doctors (user_id, crm, specialty)
+VALUES (2, '456', 'example');
