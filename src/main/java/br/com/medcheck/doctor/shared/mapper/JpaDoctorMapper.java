dcd97ef85562df32cf65c2b28caac31ca91a277d
matheus victor <matheusvictor.salles@gmail.com>
2025-02-15 10:56:36 -0300

    feat: add toDto method

--- src/main/java/br/com/medcheck/doctor/shared/mapper/JpaDoctorMapper.java
+++ src/main/java/br/com/medcheck/doctor/shared/mapper/JpaDoctorMapper.java
@@ -1,14 +1,20 @@
 package br.com.medcheck.doctor.shared.mapper;
 
-import br.com.medcheck.commom.dto.request.DoctorRequestDTO;
-import br.com.medcheck.doctor.domain.entity.JpaDoctor;
+import br.com.medcheck.doctor.app.adapter.dto.DoctorRequestBody;
+import br.com.medcheck.doctor.app.adapter.dto.DoctorResponseDTO;
+import br.com.medcheck.doctor.infrastructure.database.JpaDoctor;
 
 public class JpaDoctorMapper {
 
     private JpaDoctorMapper() {
     }
 
-    public static JpaDoctor toEntity(final DoctorRequestDTO doctor) {
-        return new JpaDoctor(doctor.name(), doctor.crm());
+    public static JpaDoctor toEntity(final DoctorRequestBody doctor) {
+        return new JpaDoctor(doctor.getName(), doctor.getCrm());
     }
+
+    public static DoctorResponseDTO toDto(final JpaDoctor doctor) {
+        return new DoctorResponseDTO(doctor.getId(), doctor.getName(), doctor.getCrm());
+    }
+
 }
